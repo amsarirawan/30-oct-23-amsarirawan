dd590fea3f276b561590721b491ec176cda8902d
Amsar <150228527+amsarirawan@users.noreply.github.com>
2023-11-09 15:19:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,9 +31,8 @@ function countBmi() {
     var bmi = Number(p[2]) / (((Number(p[1]) / 100) * Number(p[1])) / 100);
     var result = "";
     if (bmi < 18.5) {
-        result = "Underweight"
-            Anda berada dalam kategori kekurangan berat badan.
-Hubungi dokter lebih lanjut mengenai pola makan dan gizi yang baik untuk meningkatkan kesehatan.;
+        result = "Underweight<br> Anda berada dalam kategori kekurangan berat badan.
+Hubungi dokter lebih lanjut mengenai pola makan dan gizi yang baik untuk meningkatkan kesehatan</br>";
     } else if (18.5 <= bmi && bmi <= 24.9) {
         result = "Healthy";
     } else if (25 <= bmi && bmi <= 29.9) {
